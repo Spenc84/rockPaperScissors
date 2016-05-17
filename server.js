c1e6c8f8d1223306c8d3dc086ef7f2436882637d
Spencer Powell <spenc84@gmail.com>
2016-05-17 00:08:39 -0500

    corrected port number

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var app = express();
 app.use(express.static(__dirname + '/../public'));
 
 app.listen(8081, function(){
-  console.log('listening on port 8081');
+  console.log('listening on port 8084');
 });
